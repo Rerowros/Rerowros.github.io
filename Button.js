2907d862798e4221d88c422ad69b5ea2ec86e05e
Yaroslav <yp32895@gmail.com>
2024-08-25 19:56:25 +0700

    Updates

--- Button.js
+++ Button.js
@@ -10,7 +10,7 @@ class MaterialButton extends HTMLElement {
                     margin: 8px;
                     font-weight: 500;
                     color: #fff;
-                    background-color: #3f51b5;
+                    background-color: #eb6e75;
                     text-align: center;
                     text-decoration: none;
                     border: none;
@@ -20,7 +20,7 @@ class MaterialButton extends HTMLElement {
                 }
 
                 .material-button:hover {
-                    background-color: #303f9f;
+                    background-color: #c95f63;
                     box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2), 0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
                 }
 
